58344500e1ba5372e44d162d08a6284311d7ab1b
nowsilver <46918839+rosa2070@users.noreply.github.com>
2022-11-15 04:10:31 +0900

    Add files via upload

---
+++ intent/Intent1Demo/MainActivity.java
@@ -0,0 +1,23 @@
+package org.techtown.intent1demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.call){
+            Intent i = new Intent(this, SubActivity.class);
+            startActivity(i);
+        }
+    }
+}
---
+++ intent/Intent1Demo/SubActivity.java
@@ -0,0 +1,21 @@
+package org.techtown.intent1demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.exit)
+            finish();
+    }
+}
